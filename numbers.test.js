37dcc4d7bf692cf4eaa26ace0feff57241d968db
David Arroyo Menéndez <davidam@gmail.com>
2021-08-07 22:37:34 +0200

    truehood.test.js, strings.test.js, numbers.test.js, float.test.js, exceptions.test.js, arrays.test.js: creation

---
+++ numbers.test.js
@@ -0,0 +1,11 @@
+test('dos mas dos', () => {
+  const value = 2 + 2;
+  expect(value).toBeGreaterThan(3);
+  expect(value).toBeGreaterThanOrEqual(3.5);
+  expect(value).toBeLessThan(5);
+  expect(value).toBeLessThanOrEqual(4.5);
+
+  // toBe y toEqual son equivalentes para números
+  expect(value).toBe(4);
+  expect(value).toEqual(4);
+});
