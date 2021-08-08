3424a09e988274e1e1264e5f8281a387e3c68231
David Arroyo Menéndez <davidam@gmail.com>
2021-08-08 21:47:19 +0200

    array/array-iterator.test.js: fix test

--- array/array-iterator.test.js
+++ array/array-iterator.test.js
@@ -1,10 +1,12 @@
 const array1 = ['a', 'b', 'c'];
 const iterator1 = array1[Symbol.iterator]();
-
+const array2 = [];
 for (const value of iterator1) {
-  console.log(value);
+  array2.push(value);
 }
-
+test('iterator test', () => {
+    expect(array2).toEqual(["a", "b", "c"]);
+});
 // expected output: "a"
 // expected output: "b"
 // expected output: "c"
