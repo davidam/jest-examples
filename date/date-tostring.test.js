d11f958d28434b7aee9a1a2ac27d2cf731f93b4c
David Arroyo Menéndez <davidam@gmail.com>
2021-08-11 16:58:13 +0200

    date/date-tostring.test.js: creation

---
+++ date/date-tostring.test.js
@@ -0,0 +1,10 @@
+
+const event = new Date('August 19, 1975 23:15:30');
+
+test('toString test', () => {
+  expect(event.toString()).toEqual("Tue Aug 19 1975 23:15:30 GMT+0200 (hora de verano de Europa central)");
+});
+
+// console.log(event.toString());
+// expected output: Tue Aug 19 1975 23:15:30 GMT+0200 (CEST)
+// (note: your timezone may vary)
