ae0401a1a2e7b2b0903972b1960c85987135daba
David Arroyo Menéndez <davidam@gmail.com>
2021-08-11 17:01:49 +0200

    date/date-tostring.test.js: creation

--- date/date-tostring.test.js
+++ date/date-tostring.test.js
@@ -1,10 +1,9 @@
-
 const event = new Date('August 19, 1975 23:15:30');
 
 test('toString test', () => {
-  expect(event.toString()).toEqual("Tue Aug 19 1975 23:15:30 GMT+0200 (hora de verano de Europa central)");
+    expect(event.toString()).toEqual("Tue Aug 19 1975 23:15:30 GMT+0200 (hora de verano de Europa central)");
 });
 
-// console.log(event.toString());
+//console.log(event.toString());
 // expected output: Tue Aug 19 1975 23:15:30 GMT+0200 (CEST)
 // (note: your timezone may vary)
