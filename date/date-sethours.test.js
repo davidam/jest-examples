dcef57bb11b16c475ce1ff6173853a7436e4df67
David Arroyo Menéndez <davidam@gmail.com>
2021-08-11 10:41:17 +0200

    date/date-sethours.test.js: fix bug in the last commit

--- date/date-sethours.test.js
+++ date/date-sethours.test.js
@@ -4,7 +4,8 @@ test('sethours test', () => {
     event.setHours(20);
     expect(event.setHours(20)).toEqual(177704130000);
     event.setHours(20, 21, 22);
-    expect(event).toEqual(1975-08-19T18:21:22.000Z);
+    string1 = 'Tue Aug 19 1975 20:21:22 GMT+0200 (hora de verano de Europa central)';
+    expect(event.toString()).toEqual(string1);
 });
 
 // event.setHours(20);
